eba409a753076ad334ed1265abee5eb1fccc5d9d
abdelazizhanafi <abdelazizhaanafy@gmail.com>
2025-02-09 10:09:56 +0200

    - smallest pair ✅ - greedy robot ✅

---
+++ sheet3/practice/greedy_robot.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+    cin >> rows >> cols;
+    int arr[rows][cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int i = 0, j = 0, sum = 0;
+
+    while (i < rows && j < cols)
+    {
+        sum += arr[i][j];
+
+        int next_val, best_i = -1, best_j = -1;
+
+        //* is right position ok?
+        if (j + 1 < cols)
+            next_val = arr[i][j + 1], best_i = i, best_j = j + 1;
+
+        //* is bottom position ok?
+        if (i + 1 < rows)
+            if (best_i == -1 || next_val < arr[i + 1][j])
+                next_val = arr[i + 1][j], best_i = i + 1, best_j = j;
+
+        //* is diagonal position ok?
+        if (i + 1 < rows && j + 1 < cols)
+            if (best_i == -1 || next_val < arr[i + 1][j + 1])
+                next_val = arr[i + 1][j + 1], best_i = i + 1, best_j = j + 1;
+
+        if (best_i == -1)
+            break;
+
+        i = best_i, j = best_j;
+    }
+    cout << sum << endl;
+}
